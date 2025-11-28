07376fd49e030382f4632687b564a5e67645a069
Abhishek <abhishek@Abhisheks-MacBook-Air.local>
2025-11-28 18:19:51 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:lms_project/features/home/home_menu_page.dart';
 import 'package:lms_project/features/teachers/teacher_home_page.dart';
+
+import 'firebase_options.dart';
 //git add . then git commit -m "commit message" then git push
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   //await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -23,4 +26,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
